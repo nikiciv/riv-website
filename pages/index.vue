79fdd8e61316292320019c9fa8d91cd1284f5bac
nikiciv <nikiciv@gmail.com>
2020-01-19 14:57:01 -0800

    Add vue smooth scroll

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,32 @@
 <template>
-  <div>ivo</div>
+  <div class="container">
+    <a href="#div-id" v-smooth-scroll>Anchor</a>
+    <div id="div-id"></div>
+  </div>
 </template>
 
 <script>
+export default {
+  layout: "home",
+  head: {
+    title: "Home"
+  }
+};
 </script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+
+  height: 100%;
+  min-height: 300vh;
+}
+
+#div-id {
+  height: 50px;
+  width: 100px;
+  background-color: red;
+  position: relative;
+  top: 200px;
+}
+</style>
