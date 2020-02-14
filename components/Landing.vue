865351c2dbcc0c342bfebf1ec97cefe5ff7ea407
Ivo Nikic <nikiciv@gmail.com>
2020-02-14 17:27:40 +0200

    Add initial landing page

--- components/Landing.vue
+++ components/Landing.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="landing"></div>
+  <div class="landing">
+    <div class="greeting">Hello!</div>
+    <div class="title-primary">I am Ivo Nikić.</div>
+    <div class="title-secondary">I make magic happen on the Web.</div>
+  </div>
 </template>
 
 <script>
@@ -14,5 +18,22 @@ export default {};
   flex-direction: column;
   align-items: flex-start;
   min-height: 100vh;
+}
+
+.greeting {
+  color: greenyellow;
+  margin: 0 0 20px 3px;
+  font-size: 40px;
+}
+
+.title-primary {
+  font-size: 80px;
+  margin: 0;
+}
+
+.title-secondary {
+  font-size: 80px;
+  margin: 0;
+  color: red;
 }
 </style>
