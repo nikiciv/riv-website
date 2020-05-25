fe639970a64fb6f1fc9540dc3d642a72d84812aa
nikiciv <nikiciv@gmail.com>
2020-05-25 12:06:23 -0700

    Add SectionWrapper component

---
+++ components/SectionWrapper.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="section-wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<style>
+.section-wrapper {
+  padding: 150px 0;
+}
+/* Tablet */
+@media only screen and (max-width: 760px) {
+  .section-wrapper {
+    padding: 100px 0;
+  }
+}
+</style>
