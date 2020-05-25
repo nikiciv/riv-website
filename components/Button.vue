28ed982a867f0a84daa4e2c4288f70f5e895abf1
nikiciv <nikiciv@gmail.com>
2020-05-25 12:07:08 -0700

    Add custom button component

---
+++ components/Button.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="button">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.button {
+  background-color: transparent;
+  color: var(--main-text-color);
+  border: 1px solid var(--main-text-color);
+  border-radius: 3px;
+  line-height: 1;
+  text-decoration: none;
+  cursor: pointer;
+  transition: "all 0.25s cubic-bezier(0.645, 0.045, 0.355, 1)";
+  padding: 15px 20px;
+  font-size: 12px;
+}
+.button:hover {
+  background-color: var(--secondary-text-color);
+}
+.button:focus {
+  outline: none;
+}
+</style>
+
+
