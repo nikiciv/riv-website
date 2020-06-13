d8430fd53c51b861e6456e294beeb18bc1796b38
nikiciv <nikiciv@gmail.com>
2020-06-13 12:06:22 -0700

    Add monospace font family for link button

--- components/Button.vue
+++ components/Button.vue
@@ -10,6 +10,7 @@
   color: var(--main-text-color);
   border: 1px solid var(--main-text-color);
   border-radius: 3px;
+  font-family: monospace;
   text-decoration: none;
   cursor: pointer;
   line-height: 1;
