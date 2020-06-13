d228b6752dcdfda66a2d796088ce4f9be9d34776
nikiciv <nikiciv@gmail.com>
2020-06-13 11:05:32 -0700

    Remove spacing in landing component & add improvements to contact button

--- components/Landing.vue
+++ components/Landing.vue
@@ -6,57 +6,56 @@
           class="title-overline"
           :style="{ transitionDelay: '100ms' }"
           v-show="isMounted"
-        >
-          Hi, my name is
-        </div>
+        >Hi, my name is</div>
       </transition>
       <transition name="fadeup">
         <div
           class="title-primary"
           :style="{ transitionDelay: '200ms' }"
           v-show="isMounted"
-        >
-          Ivo Nikić.
-        </div>
+        >Ivo Nikić.</div>
       </transition>
       <transition name="fadeup">
-        <div
-          class="title-secondary"
-          :style="{ transitionDelay: '300ms' }"
-          v-show="isMounted"
-        >
-          <vue-typed-js
-            :strings="['code', 'JS', 'React', 'Vue', 'CSS', 'Node']"
-            :loop="true"
-          >
-            <div>I write <span class="typing"></span>.</div>
+        <div class="title-secondary" :style="{ transitionDelay: '300ms' }" v-show="isMounted">
+          <vue-typed-js :strings="['code', 'JS', 'React', 'Vue', 'CSS', 'Node']" :loop="true">
+            <div>
+              I write
+              <span class="typing"></span>.
+            </div>
           </vue-typed-js>
         </div>
       </transition>
       <transition name="fadeup">
-        <p
-          class="landing-description"
-          :style="{ transitionDelay: '400ms' }"
-          v-show="isMounted"
-        >
+        <p class="landing-description" :style="{ transitionDelay: '400ms' }" v-show="isMounted">
           I'm a software engineer specialized in building high-quality web
           applications.
         </p>
+      </transition>
+      <transition name="fadeup">
+        <div class="button-wrapper" v-show="isMounted">
+          <link-button :href="emailLink" target="_blank">Get In Touch</link-button>
+        </div>
       </transition>
     </section-wrapper>
   </div>
 </template>
 
 <script>
+import config from "~/assets/config.js";
 import SectionWrapper from "@/components/SectionWrapper";
+import Button from "@/components/Button";
+
+const { email } = config;
 
 export default {
   components: {
-    SectionWrapper
+    SectionWrapper,
+    "link-button": Button
   },
   data() {
     return {
-      isMounted: false
+      isMounted: false,
+      emailLink: `mailto:${email}`
     };
   },
   mounted() {
@@ -65,7 +64,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .landing {
   display: flex;
   justify-content: center;
@@ -74,43 +73,43 @@ export default {
   align-items: flex-start;
   min-height: 100vh;
 }
-
 .title-overline {
   color: var(--secondary-text-color);
   margin-left: 5px;
   font-size: 22px;
-  font-weight: normal;
 }
-
 .title-primary {
   color: var(--main-text-color);
   font-size: 80px;
   margin: 0;
 }
-
 .title-secondary {
   color: var(--secondary-text-color);
   font-size: 80px;
   margin: 0;
 }
-
 .landing-description {
   margin-top: 25px;
   max-width: 500px;
   margin-left: 3px;
 }
-
+.button-wrapper {
+  margin-top: 60px;
+  transition-delay: 500ms;
+}
+.button-wrapper > a {
+  font-size: 20px;
+  padding: 15px 20px;
+}
 /* Desktop */
 @media only screen and (max-width: 1000px) {
   .title-overline {
     font-size: 18px;
   }
-
   .title-primary {
     font-size: 70px;
     margin: 0;
   }
-
   .title-secondary {
     font-size: 70px;
     margin: 0;
@@ -121,12 +120,10 @@ export default {
   .title-overline {
     font-size: 18px;
   }
-
   .title-primary {
     font-size: 50px;
     margin: 0;
   }
-
   .title-secondary {
     font-size: 50px;
     margin: 0;
@@ -137,17 +134,14 @@ export default {
   .main-section {
     padding: 0 25px;
   }
-
   .title-overline {
     font-size: 16px;
     margin-left: 3px;
   }
-
   .title-primary {
     font-size: 40px;
     margin: 0;
   }
-
   .title-secondary {
     font-size: 40px;
     margin: 0;
