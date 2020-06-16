97f00ebc2fc1a8b9b94ad6b9c1f6832e44809f55
Ivo Nikić <nikiciv@gmail.com>
2020-06-16 22:20:17 +0300

    Clear vuetify options from nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,3 @@
-import colors from "vuetify/es5/util/colors";
-
 export default {
   mode: "universal",
   /*
@@ -43,34 +41,9 @@ export default {
     { src: "~/plugins/vue-typed-js", mode: "client" }
   ],
   /*
-   ** Nuxt.js dev-modules
-   */
-  buildModules: ["@nuxtjs/vuetify"],
-  /*
    ** Nuxt.js modules
    */
   modules: [],
-  /*
-   ** vuetify module configuration
-   ** https://github.com/nuxt-community/vuetify-module
-   */
-  vuetify: {
-    customVariables: ["~/assets/variables.scss"],
-    theme: {
-      dark: false,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
-  },
   /*
    ** Build configuration
    */
